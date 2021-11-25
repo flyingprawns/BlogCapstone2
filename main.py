52be3b6523095b60ab4bf733c8e8d5a2874243f7
Qijie Zhu <qzhu9@ucsc.edu>
2021-11-25 15:05:58 -0800

    added about+contact pages

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# -------- Flask Code -------- #
+# -------- Website Pages -------- #
 @app.route("/")
-def homepage():
+def home_page():
     return render_template("index.html")
 
 
+@app.route("/About")
+def about_page():
+    return render_template("about.html")
+
+
+@app.route("/Contact")
+def contact_page():
+    return render_template("contact.html")
+
+
+# ------- Entry point ------- #
 if __name__ == "__main__":
     app.run(debug=True)
